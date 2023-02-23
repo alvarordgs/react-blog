6f329cbcc06a2380b1b8842586b4c485a942a7ae
Álvaro Rodrigues <alvarordgs98@gmail.com>
2023-02-22 22:15:59 -0300

    home created, nav and posts fedd

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import Layout from './Layout'
+import About from './About'
+import Home from './Home'
+import Missing from './Missing'
+import NewPost from './NewPost'
+import PostPage from './PostPage'
+import { Route, Routes, useNavigate } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 
 function App() {
+  const [posts, setPosts] = useState([
+    {
+      id: 1,
+      title: 'Post 1',
+      datetime: "July 01, 2021 11:17:36 AM",
+      body: 'Lorem ipsum dolor sit amet'
+    },
+    {
+      id: 2,
+      title: 'Post 2',
+      datetime: "June 05, 2021 11:17:36 PM",
+      body: 'Lorem ipsum dolor sit amet'
+    },
+    {
+      id: 3,
+      title: 'Post 3',
+      datetime: "July 16, 2022 07:17:36 AM",
+      body: 'Lorem ipsum dolor sit amet'
+    }
+  ])
+  const [search, setSearch] = ('')
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout
+        search={search}
+        setSearch={setSearch}
+      />} >
+        <Route index element={<Home 
+          posts={posts}
+        />} />
+        <Route path="post">
+          <Route index element={<NewPost />} />
+          <Route path=":id" element={<PostPage />} />
+        </Route>
+        <Route path="about" element={<About />} />
+        <Route path="*" element={<Missing />} />
+      </Route>
+    </Routes>
   );
 }
 
